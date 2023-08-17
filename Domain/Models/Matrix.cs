07ba3a90f05add33c469075689567ad702d40c08
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-08-17 19:01:37 +0300

    add class Matrix

---
+++ Domain/Models/Matrix.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+//Завдання 2
+//виконайте перевантаження +
+//(для додавання матриць), – (для віднімання матриць). *
+//(множення матриць одна на одну, множення матриці на
+//число), == (перевірка матриць на рівність), != і Equals. 
+//Використовуйте механізм властивостей полів класу і ме-
+//ханізм індексаторів.
+    public class Matrix
+    {
+        private int[,] _matrix;
+        private int _rows;
+        private int _columns;
+
+        public int Rows { get => _rows; set => _rows = value; }
+        public int Columns { get => _columns; set => _columns = value; }
+
+        public Matrix() { }
+
+        public Matrix(int _rows, int _columns)
+        {
+            this._rows = _rows;
+            this._columns = _columns;
+            _matrix = new int[_rows, _columns];
+        }
+        public void InputParam()
+        {
+            Console.WriteLine("Введіть кількість рядків: ");
+            _rows = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть кількість стовпців: ");
+            _columns = int.Parse(Console.ReadLine());
+            _matrix = new int[_rows, _columns];
+            Random rand = new Random();
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _columns; j++) _matrix[i, j] = rand.Next(0, 10);
+            }
+        }
+        public void InputElem()
+        {
+            Console.WriteLine("Введіть елементи матриці: ");
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _columns; j++) _matrix[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+        public void Show()
+        {
+            Console.WriteLine("Ваша матриця: ");
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _columns; j++) Console.Write($"{_matrix[i, j]} \t");
+                Console.WriteLine();
+            }
+        }
+        public void ShowMaxElem()
+        {
+            
+            int max = _matrix[0, 0];
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _columns; j++) if (_matrix[i, j] > max) max = _matrix[i, j];
+            }
+            Console.WriteLine($"Найбільший елемент матриці: {max}");
+        }
+        public void ShowMinElem()
+        {
+            int min = _matrix[0, 0];
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _columns; j++) if (_matrix[i, j] < min) min = _matrix[i, j];
+            }
+            Console.WriteLine($"Найменший елемент матриці: {min}");
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,40 @@
-﻿namespace ConsoleApp6
+﻿using System.Text;
+using Domain.Models;
+
+namespace ConsoleApp6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+//Завдання 2
+//В одному з попередніх практичних завдань ви ство-
+//рювали клас «Матриця». Виконайте перевантаження +
+//(для додавання матриць), – (для віднімання матриць). *
+//(множення матриць одна на одну, множення матриці на
+//число), == (перевірка матриць на рівність), != і Equals.
+//Використовуйте механізм властивостей полів класу і ме -
+//ханізм індексаторів.
+
+            Console.WriteLine("Pract/Mod05/ex02\n");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Matrix matrix1 = new Matrix();
+            Console.WriteLine("Вкажіть параметри матриці 1: ");
+            matrix1.InputParam();
+            Console.WriteLine();
+            Matrix matrix2 = new Matrix();
+            Console.WriteLine("Вкажіть параметри матриці 2: ");
+            matrix2.InputParam();
+            Console.WriteLine();
+            Console.WriteLine("Матриця 1: ");
+            matrix1.Show();
+            Console.WriteLine();
+            Console.WriteLine("Матриця 2: ");
+            matrix2.Show();
+
+
         }
     }
 }
