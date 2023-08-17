ea852555a7e19edbd4cc0508af0c808c1251faeb
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-08-17 18:35:07 +0300

    pr/task01 done

---
+++ Domain/Models/Employee.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Employee
+    {
+        private string _name;
+        private string _surname;
+        private string _patronymic;
+        private DateTime _dateOfBirth;
+        private string _phoneNumber;
+        private string _email;
+        private string _position;
+        private string _jobDescription;
+        private int _salary;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Surname
+        {
+            get { return _surname; }
+            set { _surname = value; }
+        }
+        public string Patronymic
+        {
+            get { return (_patronymic is null) ? throw new ArgumentNullException("По батькові не задано") : _patronymic; }
+            set
+            {
+                if (value is null)
+                    throw new ArgumentNullException("По батькові не задано");
+                else
+                    _patronymic = value;
+            }
+        }
+        public DateTime DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set { _dateOfBirth = value; }
+        }
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set
+            {
+                if (value.Length != 12)
+                {
+                    throw new ArgumentException("Номер телефону повинен містити 12 символів");
+                }
+                else
+                {
+                    long number = Convert.ToInt64(value);
+                    _phoneNumber = number.ToString("+## (###) ###-##-##");
+                }
+            }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        public string Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+        public string JobDescription
+        {
+            get { return _jobDescription; }
+            set { _jobDescription = value; }
+        }
+
+        public int Salary
+        {
+            get { return _salary; }
+            set { _salary = value; }
+        }
+
+        public Employee() { }
+
+        public Employee(string name, string surname, string patronymic, int salary)
+        {
+            Name = name;
+            Surname = surname;
+            Patronymic = patronymic;
+            Salary = salary;
+        }
+
+        public Employee(string name, string surname, string patronymic, DateTime dateOfBirth, string phoneNumber, string email) : this()
+        {
+            Name = name;
+            Surname = surname;
+            Patronymic = patronymic;
+            DateOfBirth = dateOfBirth;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public void InputInfo()
+        {
+            Console.WriteLine("Введіть Ім'я: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введіть Прізвище: ");
+            Surname = Console.ReadLine();
+            Console.WriteLine("Введіть По батькові: ");
+            Patronymic = Console.ReadLine();
+            Console.WriteLine("Введіть дату народження: ");
+            DateOfBirth = DateTime.Parse(Console.ReadLine());
+            Console.Write("Введіть номер телефону: +");
+            PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Введіть email: ");
+            Email = Console.ReadLine();
+            Console.WriteLine("Введіть посаду: ");
+            Position = Console.ReadLine();
+            Console.WriteLine("Введіть опис обов'язків: ");
+            JobDescription = Console.ReadLine();
+            Console.WriteLine("Введіть значення зарплати: ");
+            Salary = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Інформація про співробітника:");
+            Console.WriteLine($"ПІБ: {Surname} {Name} {Patronymic}");
+            if (DateOfBirth != null && DateOfBirth != DateTime.MinValue)
+            {
+                Console.WriteLine($"Дата народження: {DateOfBirth.ToShortDateString()}");
+            }
+            if (PhoneNumber != null)
+                Console.WriteLine($"Номер телефону: {PhoneNumber}");
+            if (Email != null)
+                Console.WriteLine($"Email: {Email}");
+            if (Position != null)
+            {
+                Console.WriteLine($"Посада: {Position}");
+            }
+            if (JobDescription != null)
+                Console.WriteLine($"Опис обов'язків: {JobDescription}");
+            if (Salary != 0)
+                Console.WriteLine($"Зарплата: {Salary}");
+        }
+
+        public static int operator +(Employee employee, int value)
+        {
+            employee.Salary += value;
+            return employee.Salary;
+        }
+        public static int operator -(Employee employee, int value)
+        {
+            employee.Salary -= value;
+            return employee.Salary;
+        }
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            if (employee1.Salary == employee2.Salary)
+                return true;
+            else
+                return false;
+            
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return employee1.Salary != employee2.Salary;
+        }
+
+        public static bool operator >(Employee employee1, Employee employee2)
+        {
+            return employee1.Salary > employee2.Salary;
+        }
+        public static bool operator <(Employee employee1, Employee employee2)
+        {
+            return employee1.Salary < employee2.Salary;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Employee otherEmployee = (Employee)obj;
+            return this.Salary == otherEmployee.Salary;
+        }
+    }
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,87 @@
-﻿namespace ConsoleApp6
+﻿using System.Runtime.Intrinsics.X86;
+using System.Text;
+using Domain.Models;
+
+namespace ConsoleApp6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+//Завдання 1
+//В одному з попередніх практичних завдань ви створю-
+//вали клас «Співробітник». Додайте до вже створеного класу
+//інформацію про заробітну плату працівника.Виконайте
+//навантаження + (для збільшення зарплати на вказану кіль-
+//кість), – (для зменшення зарплати на вказану кількість), ==
+//(перевірка на рівність зарплат працівників), < і > (перевірка
+//на меншу чи більшу кількість зарплат працівників), != і
+//Equals.Використовуйте механізм властивостей полів класу.
+            Console.WriteLine("Pract/Mod05/ex01\n");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            try
+            {
+                Employee employee_1 = new Employee("Вячеслав", "Руднєв", "Михайлович", 27350);
+                Employee employee_2 = new Employee("Євген", "Вус", "Петрович", 23200);
+                Employee employee_3 = new Employee("Катерина", "Сорока", "Іванівна", 33125);
+                
+                //збільшення та зменшення зарплати працівників            
+                Console.WriteLine();
+                Console.WriteLine($"Працівник {employee_1.Name} {employee_1.Surname}, ставка {employee_1.Salary}");
+                Console.WriteLine($"Збільшення на 1000: {employee_1 + 1000}");
+                Console.WriteLine($"Зменшення на 3350: {employee_1 - 3350} \n");
+                Console.WriteLine($"Працівник {employee_2.Name} {employee_2.Surname}, ставка {employee_2.Salary}");
+                Console.WriteLine($"Збільшення на 8000: {employee_2 + 8000}");
+                Console.WriteLine($"Зменшення на 6200: {employee_2 - 6200} \n");
+                Console.WriteLine($"Працівник {employee_3.Name} {employee_3.Surname}, ставка {employee_3.Salary}");
+                Console.WriteLine($"Збільшення на 3000: {employee_3 + 3000}");
+                Console.WriteLine($"Зменшення на 2125: {employee_3 - 2125} \n");
+
+                //перевірка на рівність та нерівність зарплат працівників ==, !=
+                Console.WriteLine($"Порівняння зарплати: {employee_1 == employee_2}");
+                Console.WriteLine($"Порівняння зарплати: {employee_1 == employee_3}");
+                Console.WriteLine($"Порівняння зарплати: {employee_2 == employee_3}");
+                Console.WriteLine();
+                Console.WriteLine($"Порівняння зарплати: {employee_1 != employee_2}");
+                Console.WriteLine($"Порівняння зарплати: {employee_1 != employee_3}");
+                Console.WriteLine($"Порівняння зарплати: {employee_2 != employee_3}");
+                Console.WriteLine();
+
+                //порівняння зарплат працівників <, >
+                Console.WriteLine($"Порівняння зарплати:" +
+                    $" {((employee_1 > employee_3) ? ($"Заробітна плата у працівника {employee_1.Name} більша")
+                    : ($"Заробітна плата у працівника {employee_3.Name} більша"))}");
+                Console.WriteLine($"Порівняння зарплати:" +
+                    $" {((employee_2 < employee_3) ? ($"Заробітна плата у працівника {employee_2.Name} менша")
+                    : ($"Заробітна плата у працівника {employee_3.Name} менша"))}"); 
+                
+                Console.WriteLine();
+                //порівняння зарплат працівників через Equals
+                if (employee_1.Equals(employee_2))
+                {
+                    Console.WriteLine("Заробітні плати співпадають.");
+                }
+                else
+                {
+                    Console.WriteLine("Заробітні плати різні.");
+                }
+
+                if (employee_1.Equals(employee_3))
+                {
+                    Console.WriteLine("Заробітні плати співпадають.");
+                }
+                else
+                {
+                    Console.WriteLine("Заробітні плати різні.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
