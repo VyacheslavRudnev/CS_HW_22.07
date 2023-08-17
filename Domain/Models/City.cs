17057e13f62f81685eb1c13b9adc17403ac570f2
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-08-17 22:31:48 +0300

    practis/task03 done

---
+++ Domain/Models/City.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class City
+    {
+        //назву міста, назву країни, кількість жителів у місті,
+        //телефонний код міста, назву районів міста.
+        private string _cityName;
+        private string _countryName;
+        private int _population;
+        private string _phoneCode;
+        private string[] _districts;
+
+        public string CityName
+        {
+            get { return (_cityName is null) ? throw new ArgumentNullException("CityName is null") : _cityName; }
+            set
+            {
+                if (value is null)
+                    throw new ArgumentNullException("CityName is null");
+                else if (value.Length < 2)
+                    throw new ArgumentException("CityName must be at least 2 characters!");
+                else
+                    _cityName = value;
+            }
+        }
+        public string CountryName
+        {
+            get { return _countryName; }
+            set { _countryName = value; }
+        }
+        public int Population
+        {
+            get { return _population; }
+            set { _population = value; }
+        }
+        public string PhoneCode
+        {
+            get { return _phoneCode; }
+            set { _phoneCode = value; }
+        }
+        public string[] Districts
+        {
+            get { return _districts; }
+            set
+            {
+                if (value is null)
+                    throw new ArgumentNullException("Districts is null");
+                else
+                    _districts = value;
+            }
+        }
+            
+        public City() { }
+
+        public City(string _cityName):this()
+        {
+            this._cityName = _cityName;
+        }
+        public City(string _cityName, int _population) : this()
+        {
+            this._cityName = _cityName;
+            this._population = _population;
+        }
+        public City(string _cityName, string _countryName, int _population, string _phoneCode) : this()
+        {
+            this._cityName = _cityName;
+            this._countryName = _countryName;
+            this._population = _population;
+            this._phoneCode = _phoneCode;
+        }
+        public City(string _cityName, string _countryName, int _population, string _phoneCode, string[] _districts)
+        {
+            this._cityName = _cityName;
+            this._countryName = _countryName;
+            this._population = _population;
+            this._phoneCode = _phoneCode;
+            this._districts = _districts;
+        }
+        
+        public override string ToString()
+        {
+            return $"CityName: {_cityName}\n" +
+                   $"CountryName: {_countryName}\n" +
+                   $"Population:{_population}\n"+
+                   $"PhoneCode: {_phoneCode}\n" +
+                   $"Districts: {_districts}\n";
+        }
+        public void InputInfo()
+        {
+            Console.WriteLine("Введіть назву міста: ");
+            _cityName = Console.ReadLine();
+            Console.WriteLine("Введіть назву країни: ");
+            _countryName = Console.ReadLine();
+            Console.WriteLine("Введіть кількість жителів у місті: ");
+            _population = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введіть телефонний код міста: ");
+            _phoneCode = Console.ReadLine();
+            Console.WriteLine("Введіть назву районів міста через пробіл: ");
+            _districts = Console.ReadLine().Split(' '); 
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Назва міста: {_cityName}");
+            Console.WriteLine($"Назва країни: {_countryName}");
+            Console.WriteLine($"Кількість жителів у місті: {_population}");
+            Console.WriteLine($"Телефонний код міста: {_phoneCode}");
+            Console.WriteLine($"Назви районів міста: ");
+            for (int i = 0; i < _districts.Length; i++)
+            {
+                Console.WriteLine($"\t{i + 1}.{_districts[i]}");
+            }
+            
+        }
+        public static int operator +(City city, int value)
+        {
+            city._population += value;
+            return city._population;
+        }
+        public static int operator -(City city, int value)
+        {
+            city._population -= value;
+            return city._population;
+        }
+        public static bool operator >(City city1, City city2)
+        {
+            return city1._population > city2._population;
+        }
+        public static bool operator <(City city1, City city2)
+        {
+            return city1._population < city2._population;
+        }
+        public static bool operator ==(City city1, City city2)
+        {
+            return city1._population == city2._population;
+        }
+        public static bool operator !=(City city1, City city2)
+        {
+            return city1._population != city2._population;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is City)
+            {
+                City city = (City)obj;
+                return this._population == city._population;
+            }
+            else
+                return false;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,74 @@
-﻿namespace ConsoleApp6
+﻿using System.Text;
+using Domain.Models;
+
+namespace ConsoleApp6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+//Завдання 3
+//В одному з попередніх практичних завдань ви ство-
+//рювали клас «Місто». Виконайте перевантаження +(для
+//збільшення кількості жителів на вказану кількість), – (для
+//зменшення кількості жителів на вказану кількість), ==
+//(перевірка на рівність двох міст за кількістю жителів),
+//< і > (перевірка на меншу чи більшу кількість мешкан-
+//ців), != і Equals.Використовуйте механізм властивостей
+//полів класу.
+
+            Console.WriteLine("Pract/Mod05/ex03\n");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            City city1 = new City("Львів", 721300);
+            City city2 = new City("Київ", 2884000);
+            City city3 = new City("Хмельницький", 267000);
+
+            Console.WriteLine($"City1: {city1.CityName}, {city1.Population}");
+            Console.WriteLine($"Збільшення популяції на 100 000: {city1 + 100000}");
+            Console.WriteLine($"Зменшення популяції на 21 300: {city1 - 21300} \n");
+            Console.WriteLine($"City2: {city2.CityName}, {city2.Population}");
+            Console.WriteLine($"Збільшення популяції на 116000: {city2 + 116000}");
+            Console.WriteLine($"Зменшення популяції на 1 500 000: {city2 - 1500000} \n");
+            Console.WriteLine($"City3: {city3.CityName}, {city3.Population}");
+            Console.WriteLine($"Збільшення популяції на 600 000: {city3 + 600000}");
+            Console.WriteLine($"Зменшення популяції на 67 000: {city3 - 67000} \n");
+
+            Console.WriteLine($"Порівняння міст:" +
+                    $" {((city1 > city2) ? ($"Популяція в місті {city1.CityName} більша ніж в місті {city2.CityName}")
+                    : ($"Популяція в місті {city2.CityName} більша ніж в місті {city1.CityName}"))}");
+            Console.WriteLine($"Порівняння міст:" +
+                $" {((city2 < city3) ? ($"Популяція в місті {city2.CityName} менша ніж в місті {city3.CityName}")
+                : ($"Популяція в місті {city3.CityName} менша ніж в місті {city2.CityName}"))}");
+            Console.WriteLine();
+            Console.WriteLine($"Порівняння рівності популяції міст {city1.CityName} і {city2.CityName}: {city1 == city2}");
+            Console.WriteLine($"Порівняння рівності популяції міст {city1.CityName} і {city3.CityName}: {city1 == city3}");
+            Console.WriteLine($"Порівняння рівності популяції міст {city2.CityName} і {city3.CityName}: {city2 == city3}");
+            Console.WriteLine();
+            Console.WriteLine($"Порівняння не рівності популяції міст {city1.CityName} і {city2.CityName}: {city1 != city2}");
+            Console.WriteLine($"Порівняння не рівності популяції міст {city1.CityName} і {city3.CityName}: {city1 != city3}");
+            Console.WriteLine($"Порівняння не рівності популяції міст {city2.CityName} і {city3.CityName}: {city2 != city3}");
+            Console.WriteLine();
+            //порівняння міст через Equals
+            if (city1.Equals(city2))
+            {
+                Console.WriteLine($"Популяція міст {city1.CityName} і {city2.CityName} рівна");
+            }
+            else
+            {
+                Console.WriteLine($"Популяція міст {city1.CityName} і {city2.CityName} не рівна");
+            }
+            if (city1.Equals(city3))
+            {
+                Console.WriteLine($"Популяція міст {city1.CityName} і {city3.CityName} рівна");
+            }
+            else
+            {
+                Console.WriteLine($"Популяція міст {city1.CityName} і {city3.CityName} не рівна");
+            }
+
+
         }
     }
 }
