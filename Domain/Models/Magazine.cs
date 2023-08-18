b242a5d5cd5a1261160818e7af9f6c9f3a0e5a4b
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-08-18 21:33:38 +0300

    hw/task01 done

---
+++ Domain/Models/Magazine.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Magazine
+    {
+        private string _magazineName;
+        private int _mYear;
+        private string _mDescription;
+        private string _mPhoneNumber;
+        private string _mEmail;
+        private int _memployeeNumbers;
+
+        public string MagazineName
+        {
+            get { return _magazineName; }
+            set { _magazineName = value; }
+        }
+        public int MYear
+        {
+            get { return _mYear; }
+            set { _mYear = value; }
+        }
+        public string MDescription
+        {
+            get { return _mDescription; }
+            set { _mDescription = value; }
+        }
+        public string MPhoneNumber
+        {
+            get { return _mPhoneNumber; }
+            set 
+            {
+                if (value.Length != 12)
+                {
+                    throw new ArgumentException("Номер телефону повинен містити 12 символів");
+                }
+                else
+                {
+                    long number = Convert.ToInt64(value);
+                    _mPhoneNumber = number.ToString("+## (###) ###-##-##");
+                }
+            }
+        }
+        public string MEmail
+        {
+            get { return _mEmail; }
+            set { _mEmail = value; }
+        }
+        public int MemployeeNumbers
+        {
+            get { return _memployeeNumbers; }
+            set { _memployeeNumbers = value; }
+        }
+        public Magazine() { }
+        public Magazine(string magazineName, int memployeeNumbers)
+        {
+            MagazineName = magazineName;
+            MemployeeNumbers = memployeeNumbers;
+        }
+        public Magazine(string magazineName, int mYear, string mDescription, string mPhoneNumber, string mEmail)
+        {
+            MagazineName = magazineName;
+            MYear = mYear;
+            MDescription = mDescription;
+            MPhoneNumber = mPhoneNumber;
+            MEmail = mEmail;
+        }
+        public override string ToString()
+        {
+            return $"Magazine name: {MagazineName}\n" +
+                   $"Magazine year: {MYear}\n" +
+                   $"Magazine description: {MDescription}\n" +
+                   $"Magazine phone number: {MPhoneNumber}\n" +
+                   $"Magazine email: {MEmail}\n";
+
+        }
+
+        public void SetInfo()
+        {
+            Console.Write("Вкажіть назву журналу: ");
+            MagazineName = Console.ReadLine();
+            Console.Write("Вкажіть рік заснування журналу: ");
+            MYear = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Вкажіть опис журналу: ");
+            MDescription = Console.ReadLine();
+            Console.Write("Вкажіть контактний номер телефону журналу: + ");
+            MPhoneNumber = Console.ReadLine();
+            Console.Write("Вкажіть email адресу журналу: ");
+            MEmail = Console.ReadLine();
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Інформація про журнал: ");
+            Console.WriteLine(this);
+        }
+
+        public static Magazine operator +(Magazine magazine, int value)
+        {
+            magazine.MemployeeNumbers += value;
+            return magazine;
+        }
+
+        public static Magazine operator -(Magazine magazine, int value)
+        {
+            magazine.MemployeeNumbers -= value;
+            return magazine;
+        }
+
+        public static bool operator ==(Magazine magazine_1, Magazine magazine_2)
+        {
+            if (magazine_1.MemployeeNumbers == magazine_2.MemployeeNumbers)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator !=(Magazine magazine_1, Magazine magazine_2)
+        {
+            if (magazine_1.MemployeeNumbers != magazine_2.MemployeeNumbers)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator >(Magazine magazine_1, Magazine magazine_2)
+        {
+            return magazine_1.MemployeeNumbers > magazine_2.MemployeeNumbers;
+        }
+        public static bool operator <(Magazine magazine_1, Magazine magazine_2)
+        {
+            return magazine_1.MemployeeNumbers < magazine_2.MemployeeNumbers;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Magazine)
+            {
+                Magazine magazine = (Magazine)obj;
+                return magazine.MemployeeNumbers == this.MemployeeNumbers;
+            }
+            return false;
+        }
+       
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,67 @@
-﻿namespace ConsoleApp6
+﻿using System.Text;
+using Domain.Models;
+
+namespace ConsoleApp6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Завдання 1
+            //В одному з попередніх практичних завдань ви ство-
+            //рювали клас «Журнал». Додайте до вже створеного класу
+            //інформацію про кількість працівників.Виконайте наван-
+            //таження + (для збільшення кількості працівників на вка -
+            //зану кількість), — (для зменшення кількості працівників
+            //на вказану кількість), == (перевірка на рівність кількості
+            //працівників), < і > (перевірка на меншу чи більшу кіль -
+            //кість працівників), != і Equals.Використовуйте механізм
+            //властивостей полів класу.
+
+            Console.WriteLine("HW/Mod05/ex01\n");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Magazine magazine1 = new Magazine("Журнал Мурзілка", 100);
+            Magazine magazine2 = new Magazine("Журнал Forbs", 1000);
+            Magazine magazine3 = new Magazine("Журнал Хакер", 25);
+
+            Console.WriteLine($"{magazine1.MagazineName} має {magazine1.MemployeeNumbers} працівників");
+            magazine1 += 25;
+            Console.WriteLine($"Збільшення персоналу на 25 чоловік: {magazine1.MemployeeNumbers}");
+            magazine1 -= 100;
+            Console.WriteLine($"Зменшення персоналу на 100 чоловік: {magazine1.MemployeeNumbers}\n");
+            Console.WriteLine($"{magazine2.MagazineName} має {magazine2.MemployeeNumbers} працівників");
+            magazine2 += 250;
+            Console.WriteLine($"Збільшення персоналу на 250 чоловік: {magazine2.MemployeeNumbers}");
+            magazine2 -= 550;
+            Console.WriteLine($"Зменшення персоналу на 500 чоловік: {magazine2.MemployeeNumbers}\n");
+            Console.WriteLine($"{magazine3.MagazineName} має {magazine3.MemployeeNumbers} працівників");
+            magazine3 += 5;
+            Console.WriteLine($"Збільшення персоналу на 5 чоловік: {magazine3.MemployeeNumbers}");
+            magazine3 -= 3;
+            Console.WriteLine($"Зменшення персоналу на 3 чоловік: {magazine3.MemployeeNumbers}\n");
+            Console.WriteLine("Перевірка на рівність кількості працівників, оператор ==");
+            Console.WriteLine($"Порівняння: {magazine1 == magazine2}");
+            Console.WriteLine($"Порівняння: {magazine1 == magazine3}");
+            Console.WriteLine($"Порівняння: {magazine2 == magazine3}\n");
+            Console.WriteLine("оператор !=");
+            Console.WriteLine($"Порівняння: {magazine1 != magazine2}");
+            Console.WriteLine($"Порівняння: {magazine1 != magazine3}");
+            Console.WriteLine($"Порівняння: {magazine2 != magazine3}\n");
+            Console.WriteLine("Порівняння через метод Equals");
+            Console.WriteLine($"Порівняння: {magazine1.Equals(magazine2)}");
+            Console.WriteLine($"Порівняння: {magazine1.Equals(magazine3)}");
+            Console.WriteLine($"Порівняння: {magazine2.Equals(magazine3)}");
+            Console.WriteLine();
+            //порівняння кількості працівників <, >
+            Console.WriteLine($"Порівняння кількості працівників:" +
+                $" {((magazine1 > magazine3) ? ($"В {magazine1.MagazineName} працівників більше ніж в {magazine3.MagazineName}")
+                : ($"В {magazine3.MagazineName} працівників більше ніж в {magazine1.MagazineName}"))}");
+            Console.WriteLine($"Порівняння кількості працівників:" +
+                $" {((magazine2 < magazine3) ? ($"В {magazine2.MagazineName} працівників менше ніж в {magazine3.MagazineName}")
+                : ($"В {magazine3.MagazineName} працівників менше ніж в {magazine2.MagazineName}"))}");
+
         }
     }
 }
