a094b1a3d8bbdc5a52c2c1eea42a7c927b14234a
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-08-18 22:43:58 +0300

    HW/task02 done

---
+++ Domain/Models/Shop.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Domain.Models
+{
+    public class Shop
+    {
+        private string _shopName;
+        private string _shopAddress;
+        private string _shopDescription;
+        private string _shopPhoneNumber;
+        private string _shopEmailAddress;
+        private int _shopSquare;
+
+        public string ShopName
+        {
+            get { return _shopName; }
+            set { _shopName = value; }
+        }
+        public string ShopAddress
+        {
+            get { return _shopAddress; }
+            set { _shopAddress = value; }
+        }
+        public string ShopDescription
+        {
+            get { return _shopDescription; }
+            set { _shopDescription = value; }
+        }
+        public string ShopPhoneNumber
+        {
+            get { return _shopPhoneNumber; }
+            set
+            {
+                if (value.Length != 12)
+                {
+                    throw new ArgumentException("Номер телефону повинен містити 12 символів");
+                }
+                else
+                {
+                    long number = Convert.ToInt64(value);
+                    _shopPhoneNumber = number.ToString("+## (###) ###-##-##");
+                }
+            }
+        }
+        public string ShopEmailAddress
+        {
+            get { return _shopEmailAddress; }
+            set { _shopEmailAddress = value; }
+        }
+        public int ShopSquare
+        {
+            get { return _shopSquare; }
+            set { _shopSquare = value; }
+        }
+        public Shop() { }
+        public Shop(string shopName, int shopSquare)
+        {
+            ShopName = shopName;
+            ShopSquare = shopSquare;
+        }
+
+        public Shop(string shopName, string shopAddress, string shopDescription, string shopPhoneNumber, string shopEmailAddress)
+        {
+            ShopName = shopName;
+            ShopAddress = shopAddress;
+            ShopDescription = shopDescription;
+            ShopPhoneNumber = shopPhoneNumber;
+            ShopEmailAddress = shopEmailAddress;
+        }
+        public override string ToString()
+        {
+            return $"Магазин: {ShopName}, площа: {ShopSquare}";
+                   //$"Адреса магазину: {ShopAddress}\n" +
+                   //$"Опис магазину: {ShopDescription}\n" +
+                   //$"Телефонний номер: {ShopPhoneNumber}\n" +
+                   //$"email: {ShopEmailAddress}\n";
+                   
+        }
+        public void InputShop()
+        {
+            Console.WriteLine("Введіть назву магазину: ");
+            ShopName = Console.ReadLine();
+            Console.WriteLine("Введіть адресу магазину: ");
+            ShopAddress = Console.ReadLine();
+            Console.WriteLine("Введіть опис магазину: ");
+            ShopDescription = Console.ReadLine();
+            Console.WriteLine("Введіть номер телефону магазину: + ");
+            ShopPhoneNumber = Console.ReadLine();
+            Console.WriteLine("Введіть email магазину: ");
+            ShopEmailAddress = Console.ReadLine();
+        }
+        public void PrintShop()
+        {
+            Console.WriteLine($"\nІнформація про магазин:");
+            Console.WriteLine($"Назва магазину: {ShopName}");
+            if (ShopAddress != null)
+            {
+                Console.WriteLine($"Адреса магазину: {ShopAddress}");
+            }
+            if (ShopDescription != null)
+            {
+                Console.WriteLine($"Опис магазину: {ShopDescription}");
+            }
+            if (ShopPhoneNumber != null)
+            {
+                Console.WriteLine($"Номер телефону магазину: {ShopPhoneNumber}");
+            }
+            if (ShopEmailAddress != null)
+            {
+                Console.WriteLine($"Email магазину: {ShopEmailAddress}");
+            }
+            if (ShopSquare != 0)
+            {
+                Console.WriteLine($"Площа магазину: {ShopSquare}");
+            }
+        }
+
+        public static Shop operator +(Shop shop, int value)
+        {
+            shop.ShopSquare += value;
+            return shop;
+        }
+
+        public static Shop operator -(Shop shop, int value)
+        {
+            shop.ShopSquare -= value;
+            return shop;
+        }
+
+        public static bool operator ==(Shop shop_1, Shop shop_2)
+        {
+            if (shop_1.ShopSquare == shop_2.ShopSquare)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator !=(Shop shop_1, Shop shop_2)
+        {
+            if (shop_1.ShopSquare != shop_2.ShopSquare)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator >(Shop shop_1, Shop shop_2)
+        {
+            return shop_1.ShopSquare > shop_2.ShopSquare;
+        }
+        public static bool operator <(Shop shop_1, Shop shop_2)
+        {
+            return shop_1.ShopSquare < shop_2.ShopSquare;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Shop)
+            {
+                Shop shop = (Shop)obj;
+                return shop.ShopSquare == this.ShopSquare;
+            }
+            return false;
+        }
+
+
+
+    }
+    
+
+
+
+
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,61 @@
-﻿namespace ConsoleApp6
+﻿using System.Text;
+using Domain.Models;
+
+namespace ConsoleApp6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+//Завдання 2
+//В одному з попередніх практичних завдань ви ство-
+//рювали клас «Магазин». Додайте до вже створеного класу
+//інформацію про площу магазину.Виконайте наванта-
+//ження + (для збільшення площі магазину на вказаний
+//розмір), — (для зменшення площі магазину на вказаний
+//розмір), == (перевірка на рівність площ магазинів), < і >
+//(перевірка магазинів менших або більших за площею),
+//!= і Equals.Використовуйте механізм властивостей
+//полів класу.
+
+            Console.WriteLine("HW/Mod05/ex02\n");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Shop shop1 = new Shop("Фора", 1500);
+            Shop shop2 = new Shop("Кав'ярня", 40);
+            Shop shop3 = new Shop("Епіцентр", 25000);
+
+            Console.WriteLine($"{shop1.ShopName} має площу {shop1.ShopSquare} метрів квадратних");
+            Console.WriteLine($"Збільшення площі на 100 метрів: {shop1 + 100}");
+            Console.WriteLine($"Зменшення площі на 500 метрів: {shop1 - 500}\n");
+            Console.WriteLine($"{shop2.ShopName} має площу {shop2.ShopSquare} метрів квадратних");
+            Console.WriteLine($"Збільшення площі на 11 метрів: {shop2 + 11}");
+            Console.WriteLine($"Зменшення площі на 8 метрів: {shop2 - 8}\n");
+            Console.WriteLine($"{shop3.ShopName} має площу {shop3.ShopSquare} метрів квадратних");
+            Console.WriteLine($"Збільшення площі на 10 000 метрів: {shop3 + 10000}");
+            Console.WriteLine($"Зменшення площі на 20 000 метрів: {shop3 - 20000}\n");
+            Console.WriteLine("Перевірка на рівність площі, оператор ==");
+            Console.WriteLine($"Порівняння: {shop1 == shop2}");
+            Console.WriteLine($"Порівняння: {shop1 == shop3}");
+            Console.WriteLine($"Порівняння: {shop2 == shop3}\n");
+            Console.WriteLine("Перевірка на нерівність площі, оператор !=");
+            Console.WriteLine($"Порівняння: {shop1 != shop2}");
+            Console.WriteLine($"Порівняння: {shop1 != shop3}");
+            Console.WriteLine($"Порівняння: {shop2 != shop3}\n");
+            Console.WriteLine("Порівняння через метод Equals");
+            Console.WriteLine($"Порівняння: {shop1.Equals(shop2)}");
+            Console.WriteLine($"Порівняння: {shop1.Equals(shop3)}");
+            Console.WriteLine($"Порівняння: {shop2.Equals(shop3)}");
+            Console.WriteLine();
+            //порівняння розмірів магазинів <, >
+            Console.WriteLine($"Порівняння розмірів магазинів:" +
+                $" {((shop1 > shop2) ? ($"Площа магазину {shop1.ShopName} більша ніж площа магазину {shop2.ShopName}")
+                : ($"Площа магазину {shop2.ShopName} більша ніж площа магазину {shop1.ShopName}"))}");
+            Console.WriteLine($"Порівняння розмірів магазинів:" +
+                $" {((shop1 < shop3) ? ($"Площа магазину {shop1.ShopName} менша ніж площа магазину {shop3.ShopName}")
+                : ($"Площа магазину {shop3.ShopName} менша ніж площа магазину {shop1.ShopName}"))}");
+
         }
     }
 }
