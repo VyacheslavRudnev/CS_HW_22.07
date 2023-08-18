e7aa938ff7c65a4b8db7239184388ae3d0b07cd3
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-08-18 20:22:50 +0300

    practice/task04 done

---
+++ Domain/Models/CreditCard.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+//В одному з попередніх практичних завдань ви створю-
+//вали клас «Кредитна картка».
+//Додайте до вже створеного
+//класу інформацію про суму грошей на картці.Виконай -
+//те перевантаження + (для збільшення суми грошей на
+//вказану кількість), – (для зменшення суми грошей на
+//вказану кількість), == (перевірка на рівність CVC коду),
+//< і > (перевірка на меншу чи більшу кількість суми гро -
+//шей), != і Equals.Використовуйте механізм властивостей
+//полів класу.
+    public class CreditCard
+    {
+        private string _cardNumber;
+        private string _cvc;
+        private string _expirationDate;
+        private double _balance;
+        public string CardNumber
+        {
+            get { return _cardNumber; }
+            set
+            {
+                string cardNumberWithoutSpaces = value.Replace(" ", "");
+                if (cardNumberWithoutSpaces.Length == 16)
+                {
+                    _cardNumber = value;
+                }
+                else
+                {
+                    throw new Exception("Номер картки повинен містити 16 символів");
+                }
+            }
+        }
+        public string CVC
+        {
+            get { return _cvc; }
+            set
+            {
+                if (value.Length == 3)
+                {
+                    _cvc = value;
+                }
+                else
+                {
+                    throw new Exception("CVC повинен містити 3 символи");
+                }
+            }
+        }
+        public string ExpirationDate
+        {
+            get { return _expirationDate; }
+            set
+            {
+                if (value.Length == 5)
+                {
+                    _expirationDate = value;
+                }
+                else
+                {
+                    throw new Exception("Дата повинна містити 5 символів");
+                }
+            }
+        }
+        public double Balance
+        {
+            get { return _balance; }
+            set
+            {
+                if (value >= 0)
+                {
+                    _balance = value;
+                }
+                else
+                {
+                    throw new Exception("Баланс не може бути від'ємним");
+                }
+            }
+        }
+
+        public CreditCard() { }
+
+        public CreditCard(string number, string cvc, double balance)
+        {
+            CardNumber = number;
+            CVC = cvc;
+            Balance = balance;
+        }
+        public CreditCard(string number, string cvc, string expirationDate, double balance)
+        {
+            CardNumber = number;
+            CVC = cvc;
+            ExpirationDate = expirationDate;
+            Balance = balance;
+        }
+
+        public override string ToString()
+        {
+            return $"Картка №: {CardNumber}, " +
+                   $"CVC код: {CVC}, " +
+                   $"Поточний баланс: {Balance:f2}";
+        }
+        public static CreditCard operator +(CreditCard card, double value)
+        {
+            card.Balance += value;
+            return card;
+        }
+
+        public static CreditCard operator -(CreditCard card, double value)
+        {
+            card.Balance -= value;
+            return card;
+        }
+
+        public static bool operator ==(CreditCard card_1, CreditCard card_2)
+        {
+            if (card_1.CVC == card_2.CVC)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator !=(CreditCard card_1, CreditCard card_2)
+        {
+            if (card_1.CVC != card_2.CVC)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator >(CreditCard card_1, CreditCard card_2)
+        {
+            return card_1.Balance > card_2.Balance;
+        }
+        public static bool operator <(CreditCard card_1, CreditCard card_2)
+        {
+            return card_1.Balance < card_2.Balance;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is CreditCard)
+            {
+                CreditCard card = (CreditCard)obj;
+                return card.CVC == CVC;
+            }
+            return false;
+        }
+        //public override bool Equals(object obj)
+        //{
+        //    if (obj is CreditCard)
+        //    {
+        //        CreditCard card = (CreditCard)obj;
+        //        return card.Balance == Balance;
+        //    }
+        //    return false;
+        //}
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,78 @@
-﻿namespace ConsoleApp6
+﻿using System.Text;
+using Domain.Models;
+
+namespace ConsoleApp6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Завдання 4
+            //В одному з попередніх практичних завдань ви створю-
+            //вали клас «Кредитна картка». Додайте до вже створеного
+            //класу інформацію про суму грошей на картці.Виконай -
+            //те перевантаження + (для збільшення суми грошей на
+            //вказану кількість), – (для зменшення суми грошей на
+            //вказану кількість), == (перевірка на рівність CVC коду),
+            //< і > (перевірка на меншу чи більшу кількість суми гро -
+            //шей), != і Equals.Використовуйте механізм властивостей
+            //полів класу.
+
+            Console.WriteLine("Pract/Mod05/ex04\n");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            CreditCard card1 = new CreditCard("1234 5678 9012 3456", "123", 1000.00);
+            CreditCard card2 = new CreditCard("5678 9012 3456 7890", "123", 5000.00);
+            CreditCard card3 = new CreditCard("9012 3456 7890 1234", "333", 10000.00);
+            Console.WriteLine();
+
+            Console.WriteLine($"Карта {card1.CardNumber}, баланс {card1.Balance}");
+            Console.WriteLine($"Збільшення балансу на 5000,55: \n{card1 + 5000.55}");
+            Console.WriteLine($"Зменшення балансу на 2250,05: \n{card1 - 2250.05} \n");
+            Console.WriteLine($"Карта {card2.CardNumber}, баланс {card2.Balance}");
+            Console.WriteLine($"Збільшення балансу на 2000,10: \n{card2 + 2000.10}");
+            Console.WriteLine($"Зменшення балансу на 4500,05: \n{card2 - 4500.05} \n");
+            Console.WriteLine($"Карта {card3.CardNumber}, баланс {card3.Balance}");
+            Console.WriteLine($"Збільшення балансу на 2250,11: \n{card3 + 2250.11}");
+            Console.WriteLine($"Зменшення балансу на 10250,01: \n{card3 - 10250.01}\n");
+
+            //перевірка кодів CVC
+            Console.WriteLine($"Порівняння карток: {card1 == card2}");
+            Console.WriteLine($"Порівняння карток: {card1 == card3}");
+            Console.WriteLine();
+            Console.WriteLine($"Порівняння карток: {card1 != card2}");
+            Console.WriteLine($"Порівняння карток: {card1 != card3}");
+            Console.WriteLine();
+
+            //порівняння балансів <, >
+            Console.WriteLine($"Порівняння балансів:" +
+                $" {((card1 > card2) ? ($"Баланс на картці {card1.CardNumber} більший")
+                : ($"Баланс на картці {card2.CardNumber} більший"))}");
+            Console.WriteLine($"Порівняння балансів:" +
+                $" {((card2 > card3) ? ($"Баланс на картці {card2.CardNumber} менший")
+                : ($"Баланс на картці {card3.CardNumber} менший"))}");
+
+            //порівняння кодыв карток через Equals
+            if (card1.Equals(card2))
+            {
+                Console.WriteLine("CVC коди карток співпадають.");
+            }
+            else
+            {
+                Console.WriteLine("CVC коди карток різні.");
+            }
+
+            if (card1.Equals(card3))
+            {
+                Console.WriteLine("CVC коди карток співпадають.");
+            }
+            else
+            {
+                Console.WriteLine("CVC коди карток різні.");
+            }
+            Console.ReadKey();
+
         }
     }
 }
